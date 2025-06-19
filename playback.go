288adb7eed1e088888be467e3c1cb44b28a5b64c
AkmalFairuz <akmalf475@gmail.com>
2025-06-20 01:22:13 +0700

    Remove unused import

--- playback.go
+++ playback.go
@@ -2,7 +2,6 @@ package replay
 
 import (
 	"github.com/akmalfairuz/df-replay/action"
-	"github.com/bedrock-gophers/intercept/intercept"
 	"github.com/df-mc/dragonfly/server/block"
 	"github.com/df-mc/dragonfly/server/block/cube"
 	"github.com/df-mc/dragonfly/server/entity"
